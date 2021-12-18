498c652b46fb27d04b6549f984e8750482bcc37b
maring9 <62242566+maring9@users.noreply.github.com>
2021-12-18 20:23:26 +0200

    Lab7

--- app/src/main/java/com/example/smartwallet/MainActivity.kt
+++ app/src/main/java/com/example/smartwallet/MainActivity.kt
@@ -2,24 +2,22 @@ package com.example.smartwallet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.example.smartwallet.model.MonthlyExpenses
 import com.google.firebase.database.*
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var databaseReference : DatabaseReference
-    private lateinit var tStatus : TextView
-    private lateinit var eSearch : EditText
-    private lateinit var eIncome : EditText
-    private lateinit var eExpenses  : EditText
-    private lateinit var bSearch : Button
-    private lateinit var bUpdate : Button
-    private lateinit var currentMonth : String
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var tStatus: TextView
+    private lateinit var eSearch: EditText
+    private lateinit var eIncome: EditText
+    private lateinit var eExpenses: EditText
+    private lateinit var bUpdate: Button
+    private lateinit var spinner: Spinner
+    private lateinit var currentMonth: String
     private lateinit var databaseListener: ValueEventListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,19 +26,116 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         initComponents()
     }
 
-    private fun initComponents(){
-        tStatus = findViewById(R.id.tStatus)
-        eSearch = findViewById(R.id.eSearch)
-        eIncome = findViewById(R.id.eIncome)
-        eExpenses = findViewById(R.id.eExpenses)
-        bSearch = findViewById(R.id.bSearch)
-        bSearch.setOnClickListener(this)
-        bUpdate = findViewById(R.id.bUpdate)
-        bUpdate.setOnClickListener(this)
-
-
+    private fun initComponents() {
         val database = FirebaseDatabase.getInstance()
         databaseReference = database.reference
+        tStatus = findViewById(R.id.tStatus)
+        eIncome = findViewById(R.id.eIncome)
+        eExpenses = findViewById(R.id.eExpenses)
+        bUpdate = findViewById(R.id.bUpdate)
+        bUpdate.setOnClickListener(this)
+        spinner = findViewById(R.id.itemsSpinner)
+        populateSpinner()
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position == 0) {
+                    databaseReference.child("calendar")
+                        .child("February")
+                        .child("Income")
+                        .addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                eIncome.setText(snapshot.value.toString())
+                                spinner.setSelection(0)
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+
+                        })
+
+                    databaseReference.child("calendar")
+                        .child("February")
+                        .child("Expenses")
+                        .addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                eExpenses.setText(snapshot.value.toString())
+                                spinner.setSelection(0)
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+
+                        })
+                } else if (position == 1) {
+                    databaseReference.child("calendar")
+                        .child("January")
+                        .child("Income")
+                        .addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                eIncome.setText(snapshot.value.toString())
+                                spinner.setSelection(1)
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+
+                        })
+
+                    databaseReference.child("calendar")
+                        .child("January")
+                        .child("Expenses")
+                        .addValueEventListener(object : ValueEventListener {
+                            override fun onDataChange(snapshot: DataSnapshot) {
+                                eExpenses.setText(snapshot.value.toString())
+                                spinner.setSelection(1)
+                            }
+
+                            override fun onCancelled(error: DatabaseError) {
+                                TODO("Not yet implemented")
+                            }
+
+                        })
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
+
+    }
+
+    private fun populateSpinner() {
+        databaseReference
+            .child("calendar")
+            .addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val months = ArrayList<String>()
+                    for (data in snapshot.children) {
+                        months.add(data.key.toString())
+                    }
+                    spinner.adapter = ArrayAdapter<String>(
+                        this@MainActivity,
+                        android.R.layout.simple_list_item_1,
+                        months
+                    )
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    TODO("Not yet implemented")
+                }
+            })
+
     }
 
     private fun createNewDBListener() {
@@ -71,41 +166,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     }
 
 
-
     override fun onClick(v: View?) {
-        when (v!!.id){
-            R.id.bSearch -> searchLogic()
+        when (v!!.id) {
             R.id.bUpdate -> updateLogic()
         }
     }
 
-    private fun searchLogic(){
-        if(eSearch.text.isEmpty()) Toast.makeText(this,"The field must be filled",Toast.LENGTH_SHORT)
-            .show()
-        else{
-            currentMonth = eSearch.text.toString().lowercase()
-            tStatus.text = "Searching..."
-            createNewDBListener()
-        }
-    }
+    private fun updateLogic() {
+        currentMonth = spinner.selectedItem.toString()
+        val selectedItem = spinner.selectedItemPosition
+        val monthlyExpenses = MonthlyExpenses(
+            currentMonth,
+            eExpenses.text.toString().toDouble(),
+            eIncome.text.toString().toDouble()
+        )
+        databaseReference.child("calendar")
+            .child(monthlyExpenses.month)
+            .child("Income")
+            .setValue(monthlyExpenses.income)
 
-    private fun updateLogic(){
-        if(eSearch.text.isEmpty() || eIncome.text.isEmpty() || eExpenses.text.isEmpty())
-            Toast.makeText(this,"The fields must be filled",Toast.LENGTH_SHORT)
-                .show()
-        else{
-            val monthlyExpenses = MonthlyExpenses(eSearch.text.toString(),eExpenses.text.toString().toInt(),eIncome.text.toString().toInt())
-            databaseReference
-                .child("calendar")
-                .child(monthlyExpenses.month)
-                .child("Expenses")
-                .setValue(monthlyExpenses.expenses)
-
-            databaseReference
-                .child("calendar")
-                .child(monthlyExpenses.month)
-                .child("Income")
-                .setValue(monthlyExpenses.income)
-        }
+        databaseReference.child("calendar")
+            .child(monthlyExpenses.month)
+            .child("Expenses")
+            .setValue(monthlyExpenses.expenses)
     }
 }
